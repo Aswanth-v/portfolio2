2d7823bf783b11feb2eeaaf1b972f4a47f507015
Aswabnth.v <aswanthv695@gmail.com>
2025-07-30 09:09:40 -0700

    added changes on resume deinge

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Navbar.css';
-
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faInstagram, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
@@ -13,18 +12,25 @@ const Navbar = () => {
     deleteSpeed: 50,
   });
 
-  // 🔽 Resume download function
   const handleDownloadResume = () => {
-    const resumeUrl = '/recorces/Aswanth_Resume.pdf';
+    const resumeUrl = '/recorces/Aswanth_Resume.pdf'; // Make sure file is in public/resources
     const link = document.createElement('a');
     link.href = resumeUrl;
     link.download = 'Aswanth_Resume.pdf';
+    document.body.appendChild(link);
     link.click();
+    document.body.removeChild(link);
   };
 
   return (
     <>
       <div className="background">
+
+        {/* Resume Download Button */}
+        <button className="resume-button" onClick={handleDownloadResume}>
+          Download Resume
+        </button>
+
         <div className="overlay-text">
           <p style={{ color: "#1ce480", fontFamily: "Georgia" }}>Hi! &nbsp;I am</p>
           <h1>Aswanth.v</h1>
@@ -42,11 +48,6 @@ const Navbar = () => {
             >
               About Me
             </button>
-
-            {/* 🔽 Resume Download Button */}
-            <button className="button" onClick={handleDownloadResume}>
-              Download Resume
-            </button>
           </div>
         </div>
 
@@ -60,15 +61,12 @@ const Navbar = () => {
           <a href="https://github.com/aswanth-v" target="_blank" rel="noopener noreferrer" className="icon1">
             <FontAwesomeIcon icon={faGithub} />
           </a>
-
           <a href="https://linkedin.com/in/aswanth-v" target="_blank" rel="noopener noreferrer" className="icon2">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
-
           <a href="https://instagram.com/_aswa_v" target="_blank" rel="noopener noreferrer" className="icon3">
             <FontAwesomeIcon icon={faInstagram} />
           </a>
-
           <a href="https://wa.me/7736212900" target="_blank" rel="noopener noreferrer" className="icon4">
             <FontAwesomeIcon icon={faWhatsapp} />
           </a>
